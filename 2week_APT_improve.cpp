785fd5edbebb2df26e1729e0dca42dbc71fe9b61
Hanjung <hanjungv@gmail.com>
2016-03-10 11:34:29 +0900

    Update 2week_APT_improve.cpp

--- 2week_APT_improve.cpp
+++ 2week_APT_improve.cpp
@@ -17,16 +17,13 @@ int main(){
 				scanf("%d", &add[i]);
 				Total += add[i];
 			}
-			bool isZero = false;
 			for (int k = M - 1; k > 0; k--){
 				int new_add = arr[k - 1], last = arr[k];
 				for (int i = 0; i < M - 1; i++){
 					int comp = add[i];
 					add[i] = min(comp, min(new_add, last));
-					if (add[i] == 0){
-						isZero = true;
+					if (add[i] == 0)
 						break;
-					}
 					Total += add[i];
 					last = comp;
 					new_add = add[i];
